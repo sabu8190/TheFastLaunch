7f5a47762cc8dc252cb5446a46aa662ab3f68271
sabu8190 <sabu8190@users.noreply.github.com>
2026-08-28 23:12:59 +0900

    release: Release v-b1.3 with JEF (Misanthropy) and Tinkers Prefilter (MikhaXOXO) optimizations and credits

--- src/main/java/com/fastlaunch/FastLaunchForgeMod.java
+++ src/main/java/com/fastlaunch/FastLaunchForgeMod.java
@@ -25,7 +25,7 @@ import java.io.File;
 public class FastLaunchForgeMod {
     public static final String MOD_ID = "fastlaunch";
     public static final String MOD_NAME = "TheFastLaunch";
-    public static final String VERSION = "b1.2";
+    public static final String VERSION = "b1.3";
     private static final Logger LOGGER = LogManager.getLogger("FastLaunch/Core");
 
     static {
